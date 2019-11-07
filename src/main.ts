e40f70cc77f8c22b280984409658bb9008b81d37
Chau Giang <neversmile@outlook.com>
2019-11-07 10:35:22 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 
 
 async function bootstrap() {
-  Sentry.init({ dsn: 'https://f26e5129520f4afd9a2f878e3e3b9546@sentry.io/1798077' });
+  Sentry.init({ dsn: 'your dsn' });
 
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
